e974d472258528b445ca2d4bffa3ec4c567d1389
Mike MacMillan <macmillanmikej@gmail.com>
2012-03-11 22:06:41 -0400

    Added index file with basic PHP output

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+ <head>
+  <title>Loocid v1.0 - Simple Scrum & Kanban Tracking Tool</title>
+ </head>
+ <body>
+<hr />
+Below is information for debugging and PHP validation:
+<?php phpinfo(); ?>
+
+ </body>
+</html>
